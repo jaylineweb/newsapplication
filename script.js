cc99e3e3badeb959b13a928775a94a6904d43e25
jaylineweb <darkjay118@gmail.com>
2024-07-10 00:39:07 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let items = [
-    { name: "apple", price: 50 },
-    { name: "banana", price: 30 },
-    { name: "cherry", price: 20 }
-];
-
-// 가격에 따라 오름차순 정렬
-items.sort((a, b) => a.price - b.price);
-console.log(items);
