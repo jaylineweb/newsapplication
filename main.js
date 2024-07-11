554e9238fd3d3ecdeb1ffbcb0e80e4c82ac5dd3a
jaylineweb <darkjay118@gmail.com>
2024-07-11 13:25:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //let apiKey = 'be3e9aaf141040b68a633150eff989ad';
 let newsList = []; // 'news'를 다른 함수에서도 쓸 수 있게 전역 변수로 선언해주기
+let searchContainer = document.querySelector(".search-container");
+let searchButton = document.querySelector(".search-button");
+let inputArea = document.querySelector(".input-area");
 const menus = document.querySelectorAll('.menus button');
-console.log('mmm',menus); //NodeList(7) [button, button, button, button, button, button, button] length:7
+let keyword = ''; // 검색 키워드 변수 추가
+let navBar = document.querySelector('.menus-container');
 
 menus.forEach((menu)=>{
     menu.addEventListener('click',(event)=>{
@@ -9,31 +13,66 @@ menus.forEach((menu)=>{
     })
 })
 
+searchButton.addEventListener("click", setKeywords);
+
+// 윈도우 리사이즈 이벤트 리스너 추가
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 991) {
+        closeMenu();
+    }
+});
+
+async function setKeywords() {
+    if (inputArea.value == "") {
+        alert("검색할 내용을 입력해주세요.");
+        return;
+    }
+    keyword = `&q=${inputArea.value}`;
+    inputArea.value = "";
+
+    await getLatestNews();
+    if (newsList.length === 0) {
+        document.getElementById('news-board').innerHTML = `
+            <div class="alert alert-danger" role="alert">
+                No result for this search.
+            </div>
+        `;
+    }
+}
+
+function navBarActivate() {
+    navBar.classList.toggle('active');
+}
+
+function searchIconActivate(){
+    searchContainer.classList.toggle("active");
+}
+
+function closeMenu() {
+    navBar.classList.remove('active');
+}
+
 const getLatestNews = async () => {
-    const url = new URL(`https://apisuccess.netlify.app/top-headlines?country=kr`);
+    const url = new URL(`https://apisuccess.netlify.app/top-headlines?country=kr${keyword}`);
     const response = await fetch(url);
     const data = await response.json();
     newsList = data.articles; // 뉴스 기사들을 news 변수에 재할당
     render();
-    console.log('ddddd',newsList);
 };
 
 const getNewsByCategory= async (event)=>{
-    const category = event.target.textContent.toLowerCase(); //toLowerCase() => 대문자를 소문자로 바꿔버림.
-    console.log('category',category);
-    const url = new URL(
-        `https://apisuccess.netlify.app/top-headlines?country=kr&category=${category}`
-    );
+    keyword = ''; // 카테고리 선택 시 검색 키워드 초기화
+    const category = event.target.textContent.toLowerCase();
+    const url = new URL(`https://apisuccess.netlify.app/top-headlines?country=kr&category=${category}`);
     const response = await fetch(url);
     const data = await response.json();
-    console.log('ddd',data);
     newsList = data.articles;
     render();
 };
 
 const render = () => {
     const newsHTML = newsList.map((news) => {
-        const publishedDate = new Date(news.publishedAt).toISOString().split('T')[0]; //2024-02-16T16:40:00Z 부분을 날짜 영역까지만 출력해주기
+        const publishedDate = new Date(news.publishedAt).toISOString().split('T')[0];
         const newsItem = `
             <div class="row news">
                 <div id="newsImg" class="col-lg-4">
@@ -52,9 +91,4 @@ const render = () => {
     document.getElementById('news-board').innerHTML = newsHTML;
 }
 
-
 getLatestNews(); // 함수를 호출하여 뉴스 데이터를 가져오고 동적으로 HTML에 삽입한다.
-
-//1.버튼들에 클릭이벤트 주기
-//2.카테고리별 뉴스 가져오기
-//3. 그 뉴스를 보여주기
