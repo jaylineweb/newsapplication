f811be7d74df84002d2fa11e4dcbb28256058f70
jaylineweb <darkjay118@gmail.com>
2024-07-12 17:56:18 +0900

    Update main_refactoring.js

--- main_refactoring.js
+++ main_refactoring.js
@@ -51,6 +51,9 @@ async function fetchNews({ category = '', keyword = '' } = {}) {
     const url = new URL(`https://apisuccess.netlify.app/top-headlines?country=kr${category ? `&category=${category}` : ''}${keyword}`);
     console.log('Request URL:', url.toString()); // URL을 콘솔에 출력하여 확인
     try {
+        url.searchParams.set('page',page); // => &page=page
+        url.searchParams.set('pageSize',pageSize);
+
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -106,21 +109,35 @@ const paginationRender=()=>{
     //page
     //pageSize
     //groupSize
+    //totalPages
+    const totalPages = Math.ceil(totalResults / pageSize);
     //pageGroup
     const pageGroup = Math.ceil(page / groupSize);
+
     //lastPage
-    const lastPage = pageGroup * groupSize;
+    let lastPage = pageGroup * groupSize;
+    //마지막 페이지그룹이 그룹사이즈보다 작다? lastPage = totalPage
+    if(lastPage > totalPages){
+        lastPage = totalPages
+    }
+
     //firstPage
-    const firstPage = lastPage - (groupSize - 1);
+    const firstPage = lastPage - (groupSize - 1) <=0? 1:lastPage - (groupSize - 1);
     //first~last
 
     let paginationHTML = ``;
 
     for(let i=firstPage;i<=lastPage;i++){
-        paginationHTML+=`<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+        paginationHTML+=`<li class="page-item ${i===page?"active":''}" onclick="moveToPage(${i})"><a class="page-link" href="#n">${i}</a></li>`;
     }
 
     document.querySelector('.pagination').innerHTML = paginationHTML;
+}
+
+const moveToPage =(pageNum)=>{
+    console.log('moveToPage',pageNum);
+    page = pageNum;
+    fetchNews();
 }
 
 // Moment.js를 로드하기 위한 스크립트 추가
